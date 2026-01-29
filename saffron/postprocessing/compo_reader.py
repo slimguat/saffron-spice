Fix radiance (rad) calculation and header logic
- Correct radiance formula to use sqrt(2π), WID in nm, and INT in W/sr/m²/nm.
- Ensure unit consistency in rad calculation.
- Add logic to use INT header if RAD header is missing when generating maps.
- Update error propagation for rad_err accordingly.
- Add clarifying comments for future maintenance.

saffron/postprocessing/compo_reader.py
@@ -21,6 +21,8 @@ from collections.abc import Iterable
 from sunpy.map import GenericMap
 import pandas as pd
 from saffron.fit_models import ModelFactory
+import warnings
+from astropy.units import UnitsWarning
 
 # from euispice_coreg.hdrshift.alignment import Alignment
 # from euispice_coreg.plot.plot import PlotFunctions
@@ -283,7 +285,8 @@ class SPECLine:
                 data[:,:100] = np.nan
                 data[:,700:] = np.nan
         
-        _map = Map(data, self.headers[param if "rad" not in param else "int"])
+        # _map = Map(data, self.headers[param if "rad" in param else "int"])
+        _map = Map(data, self.headers.get(param,'int'))
         if param in ["int", "rad", "wid"] or "err" in param:
             _map.plot_settings["cmap"] = "magma" if "err" not in param else "gray"
             _map.plot_settings["norm"] = normit(
@@ -521,10 +524,14 @@ class SPECLine:
             self._all["rad"] = [None,None]
             self._all["rad_err"] = [None,None]
             
-            self._all["rad"][0] = self["int"] * self["wid"] * np.sqrt(np.pi)
-            self._all["rad_err"][0] = (
-                self["int_err"] / self["int"] + self["wid_err"] / self["wid"]
-            ) * self["rad"]
+            # self._all["rad"][0] = self["int"] * self["wid"] * np.sqrt(np.pi)
+            with warnings.filterwarnings("ignore", category=UnitsWarning):
+                self._all["rad"][0] = (
+                    (u.Unit(self.headers['int']["BUNIT"]) * self["int"]).to(u.W / (u.m**2 * u.sr* u.nm)) * 
+                    (u.Unit(self.headers['wid']["BUNIT"]) * self["wid"]).to(u.nm)  ).to(u.W / (u.m**2 * u.sr)).value * np.sqrt(2* np.pi)
+                self._all["rad_err"][0] = (
+                    self["int_err"] / self["int"] + self["wid_err"] / self["wid"]
+                ) * self["rad"]
             
             self._all["rad"]    [1] = self.headers["int"].copy()
             self._all["rad_err"][1] = self.headers["int_err"].copy()